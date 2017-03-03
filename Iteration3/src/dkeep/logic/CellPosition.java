55cf17e25e065ee10595754333cf595604bf2065
Zé <Z@ssla.fe.up.pt>
2017-03-03 11:25:01 +0000

    CellPosition implemented

--- Iteration3/src/dkeep/logic/CellPosition.java
+++ Iteration3/src/dkeep/logic/CellPosition.java
@@ -1,5 +1,19 @@
 package dkeep.logic;
 
 public class CellPosition {
-
+	int [] position;
+	
+	public CellPosition(int y, int x){
+		position[0] = y;
+		position[1] = x;
+	}
+	
+	public void setCellPosition(int y, int x){
+		position[0] = y;
+		position[1] = x;
+	}
+	
+	public int [] getCellPosition(){
+		return position;
+	}
 }
