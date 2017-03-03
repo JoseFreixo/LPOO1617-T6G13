fa125a92853b4c2e152cb00adc55246b91e67ea8
Zé <Z@ssla.fe.up.pt>
2017-03-03 12:37:05 +0000

    Ogre and Gaurd good

--- Iteration3/src/dkeep/logic/Guard.java
+++ Iteration3/src/dkeep/logic/Guard.java
@@ -1,8 +1,15 @@
 package dkeep.logic;
 
-public abstract class Guard extends Unit {
+import java.util.Random;
+
+public class Guard extends Unit {
+	int type; // 0 -> Rookie | 1 -> Drunken | 2 -> Suspicious
 	
 	public Guard(int y, int x) {
 		super(y, x);
+		Random rand = new Random();
+		type = rand.nextInt(3);
 	}
+	
+	
 }
---
+++ Iteration3/src/dkeep/logic/Ogre.java
@@ -0,0 +1,9 @@
+package dkeep.logic;
+
+public class Ogre extends Unit{
+
+	public Ogre(int y, int x) {
+		super(y, x);
+	}
+
+}
