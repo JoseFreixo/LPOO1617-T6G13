e6011ad5c3f1a307770b8f47ca6f0c6defb5469c
up201405612 <up201405612@B03AV1134.salas.feupsig.fe.up.pt>
2017-03-03 12:55:38 +0000

    some functions on Class Game

--- Iteration3/src/dkeep/logic/Game.java
+++ Iteration3/src/dkeep/logic/Game.java
@@ -1,18 +1,23 @@
 package dkeep.logic;
 
+import java.util.ArrayList;
+
 public class Game {
 	GameMap map;
 	Hero heroi;
 	Guard guarda;
+	ArrayList<Ogre> ogres = new ArrayList<Ogre>();
 	
 	public Game(GameMap map){
 		this.map = map;
+		
 		
 		for (int i=0;i<map.getMap().length;i++){
 			for (int j=0; j<map.getMap()[i].length;j++){
 				switch(map.getMap()[i][j]){
 				case 'H': heroi= new Hero(i,j);
 				case 'G': guarda = new Guard(i,j);
+				case 'O': ogres.add(new Ogre(i,j));
 				}
 			}
 		}
@@ -24,7 +29,11 @@ public class Game {
 	
 	public boolean isGameOver(){
 		boolean lost=false;
-		//.......
+		
 		return lost;
+	}
+	
+	public boolean isValid(CellPosition posheroi, CellPosition posGuard){
+		return true;
 	}
 }
