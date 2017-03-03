90c6ecc474e9aac289be704b54f6d60fdb08c8df
up201405612 <up201405612@B03AV1134.salas.feupsig.fe.up.pt>
2017-03-03 12:35:45 +0000

    Starting game class

--- Iteration3/src/dkeep/logic/Game.java
+++ Iteration3/src/dkeep/logic/Game.java
@@ -4,12 +4,27 @@ public class Game {
 	GameMap map;
 	Hero heroi;
 	Guard guarda;
-	Ogre ogre;
-	boolean g;
-	boolean o;
 	
 	public Game(GameMap map){
 		this.map = map;
-			
+		
+		for (int i=0;i<map.getMap().length;i++){
+			for (int j=0; j<map.getMap()[i].length;j++){
+				switch(map.getMap()[i][j]){
+				case 'H': heroi= new Hero(i,j);
+				case 'G': guarda = new Guard(i,j);
+				}
+			}
+		}
+	}
+	
+	public CellPosition getHeroPosition(){
+		return heroi.getPosition();
+	}
+	
+	public boolean isGameOver(){
+		boolean lost=false;
+		//.......
+		return lost;
 	}
 }
