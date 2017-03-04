2ffda2bb0d214ba53d97d687005461be7ea52ca8
Zé <Z@Z-Freixo>
2017-03-04 16:24:14 +0000

    Guard fixed movement

--- Iteration3/src/dkeep/logic/Guard.java
+++ Iteration3/src/dkeep/logic/Guard.java
@@ -4,6 +4,12 @@ import java.util.Random;
 
 public class Guard extends Unit {
 	int type; // 0 -> Rookie | 1 -> Drunken | 2 -> Suspicious
+	char [] movement = {'A', 
+			'S', 'S', 'S', 'S', 
+			'A', 'A', 'A', 'A', 'A', 'A', 
+			'S', 
+			'D', 'D', 'D', 'D', 'D', 'D', 'D', 
+			'W', 'W', 'W', 'W', 'W'};
 	
 	public Guard(int y, int x) {
 		super(y, x);
