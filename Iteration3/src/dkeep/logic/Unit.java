55a76b177584ca80146350a55183102bc051a8d6
Zé <Z@Z-Freixo>
2017-03-10 11:24:00 +0000

    Representation added.

--- Iteration3/src/dkeep/logic/Unit.java
+++ Iteration3/src/dkeep/logic/Unit.java
@@ -2,11 +2,21 @@ package dkeep.logic;
 
 public abstract class Unit {
 	CellPosition posicao;
+	char representation;
 	
-	public Unit(int y, int x){
+	public Unit(int y, int x, char rep){
 		posicao = new CellPosition(y, x);
+		representation = rep;
 	}
 	
+	public char getRepresentation() {
+		return representation;
+	}
+
+	public void setRepresentation(char representation) {
+		this.representation = representation;
+	}
+
 	public void setPosition(int y, int x){
 		posicao.setCellPosition(y, x);
 	}
