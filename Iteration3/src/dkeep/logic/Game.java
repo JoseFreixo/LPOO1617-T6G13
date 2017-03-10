3f89d84a961ec6a621ffbf69a8fdc051cf0544a0
Zé <Z@Z-Freixo>
2017-03-10 11:33:54 +0000

    Lever added and game changed

--- Iteration3/src/dkeep/logic/Game.java
+++ Iteration3/src/dkeep/logic/Game.java
@@ -5,23 +5,27 @@ import java.util.ArrayList;
 public class Game {
 	public static final boolean DEFEAT = true;
 	Boolean Status;
+	boolean openDoors;
 	GameMap map;
 	Hero heroi;
 	Guard guarda;
+	Lever lever;
 	ArrayList<Ogre> ogres = new ArrayList<Ogre>();
 	
 	public Game(GameMap map){
 		this.map = map;
 		Status=null;
-		
+		openDoors = false;
 		for (int i=0;i<map.getMap().length;i++){
 			for (int j=0; j<map.getMap()[i].length;j++){
 				switch(map.getMap()[i][j]){
-				case 'H': heroi= new Hero(i,j);
+				case 'H': heroi = new Hero(i, j, 'H');
 				break;
-				case 'G': guarda = new Guard(i,j);
+				case 'G': guarda = new Guard(i, j, 'G');
 				break;
-				case 'O': ogres.add(new Ogre(i,j));
+				case 'O': ogres.add(new Ogre(i, j, 'O'));
+				break;
+				case 'k': lever = new Lever(i, j, 'k');
 				break;
 				}
 			}
@@ -70,27 +74,39 @@ public boolean HeroCaught(CellPosition Guard_or_OgrePos, CellPosition HeroPos){
 		return false;
 	}
 
-	public void moveHero(char c) { 
-		c = Character.toUpperCase(c);
-		if (c == 'W' && map.validPosition(getHeroPosition().getY() - 1, getHeroPosition().getX())){
-			map.setUnitPosMap(new CellPosition(getHeroPosition().getY() - 1,getHeroPosition().getX()), getHeroPosition(), 'H');
-			heroi.setPosition(getHeroPosition().getY() - 1, getHeroPosition().getX());
-		}
-		else if (c == 'S' && map.validPosition(getHeroPosition().getY() + 1, getHeroPosition().getX())){
-			map.setUnitPosMap(new CellPosition(getHeroPosition().getY() + 1, getHeroPosition().getX()), getHeroPosition(), 'H');
-			heroi.setPosition(getHeroPosition().getY() + 1, getHeroPosition().getX());
-		}
-		else if (c == 'A' && map.validPosition(getHeroPosition().getY(), getHeroPosition().getX() - 1)){
-			map.setUnitPosMap(new CellPosition(getHeroPosition().getY(), getHeroPosition().getX() - 1), getHeroPosition(), 'H');
-			heroi.setPosition(getHeroPosition().getY(), getHeroPosition().getX() - 1);
-			}
-		else if (c == 'D' && map.validPosition(getHeroPosition().getY(), getHeroPosition().getX() + 1)){
-			map.setUnitPosMap(new CellPosition(getHeroPosition().getY(), getHeroPosition().getX() + 1), getHeroPosition(), 'H');
-			heroi.setPosition(getHeroPosition().getY(), getHeroPosition().getX() + 1);
-			}
+public void moveHero(char c) { 
+	c = Character.toUpperCase(c);
+	int y = heroi.getPosition().getY();
+	int x = heroi.getPosition().getX();
+	
+	if (c == 'W')
+		y -= 1;
+	else if (c == 'S')
+		y += 1;
+	else if (c == 'A')
+		x -= 1;
+	else if (c == 'D')
+		x += 1;
+	else
+		return;
+	
+	if (new CellPosition(y, x).equals(lever.getPosition()))
+		heroi.setRepresentation('K');
+	
+	if (map.validPosition(y, x)){
+		map.setUnitPosMap(new CellPosition(y, x), getHeroPosition(), heroi.getRepresentation());
+		heroi.setPosition(y, x);
 	}
+	
+	if (map.getMap()[y][x] == 'I' && heroi.getRepresentation() == 'K')
+		map.openDoors();
+}
 
 	public boolean EndStatus() {
 		return Status;
+	}
+	
+	public boolean areDoorsOpen(){
+		return openDoors;
 	}
 }
