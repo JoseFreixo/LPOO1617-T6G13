1ec92c5b2cd2ffbe17af0d0ea3c4bf79d7371694
r_tor <r_tor@DESKTOP-HQPJ9KM.home>
2017-03-26 13:36:21 +0100

    Trying something, hope it works

--- Iteration5/src/dkeep/logic/Game.java
+++ Iteration5/src/dkeep/logic/Game.java
@@ -103,23 +103,33 @@ public class Game implements java.io.Serializable {
 	}
 
 	public boolean HeroCaught(CellPosition Guard_or_OgrePos, CellPosition HeroPos){
-
-		if(Guard_or_OgrePos.getX()==HeroPos.getX()-1&& Guard_or_OgrePos.getY()==HeroPos.getY()){
-			return true;
-		}
-		if(Guard_or_OgrePos.getX()==HeroPos.getX()+1&& Guard_or_OgrePos.getY()==HeroPos.getY()){	
-			return true;
-		}
-		if(Guard_or_OgrePos.getX()==HeroPos.getX()&& Guard_or_OgrePos.getY()==HeroPos.getY()+1){
-			return true;
-		}
-		if(Guard_or_OgrePos.getX()==HeroPos.getX()&& Guard_or_OgrePos.getY()==HeroPos.getY()-1){
-			return true;
-		}		
-		if(Guard_or_OgrePos.getX()==HeroPos.getX()&& Guard_or_OgrePos.getY()==HeroPos.getY()){
-			return true;
-		}
+		if(Guard_or_OgrePos.getY()==HeroPos.getY())
+			if(Guard_or_OgrePos.getX()==HeroPos.getX()-1||Guard_or_OgrePos.getX()==HeroPos.getX()+1||Guard_or_OgrePos.getX()==HeroPos.getX())
+				return true;
+		
+		if(Guard_or_OgrePos.getX()==HeroPos.getX())
+			if(Guard_or_OgrePos.getY()==HeroPos.getY()+1||Guard_or_OgrePos.getY()==HeroPos.getY()-1)
+				return true;
+		
 		return false;
+						
+////
+////		if(Guard_or_OgrePos.getX()==HeroPos.getX()-1&& Guard_or_OgrePos.getY()==HeroPos.getY()){
+////			return true;
+////		}
+////		if(Guard_or_OgrePos.getX()==HeroPos.getX()+1&& Guard_or_OgrePos.getY()==HeroPos.getY()){	
+////			return true;
+////		}
+//		if(Guard_or_OgrePos.getX()==HeroPos.getX()&& Guard_or_OgrePos.getY()==HeroPos.getY()+1){
+//			return true;
+//		}
+//		if(Guard_or_OgrePos.getX()==HeroPos.getX()&& Guard_or_OgrePos.getY()==HeroPos.getY()-1){
+//			return true;
+//		}		
+////		if(Guard_or_OgrePos.getX()==HeroPos.getX()&& Guard_or_OgrePos.getY()==HeroPos.getY()){
+////			return true;
+////		}
+//		return false;
 	}
 
 	public void MoveAllTheOgres(){	
